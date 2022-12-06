ccf0d89d94405f877a3eb94f070f16f205512fb1
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2022-12-06 17:29:53 +0100

    ✨ Add MQ131 sensor

--- src/TTGO_T-Beam_LoRa_APRS.ino
+++ src/TTGO_T-Beam_LoRa_APRS.ino
@@ -271,12 +271,34 @@ BG_RF95 rf95(18, 26); // TTGO T-Beam has NSS @ Pin 18 and Interrupt IO @ Pin26
 #define OLED_RESET 4 // not used
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
+#include "MQ131.h"
+
 // +---------------------------------------------------------------------+//
 // + SETUP --------------------------------------------------------------+//
 // +---------------------------------------------------------------------+//
 
 void setup()
 {
+  Serial.begin(115200);
+  // Init the sensor
+  // - Heater control on pin 2
+  // - Sensor analog read on pin A0
+  // - Model LOW_CONCENTRATION
+  // - Load resistance RL of 1MOhms (1000000 Ohms)
+  MQ131.begin(2, A0, LOW_CONCENTRATION, 1000000);
+
+  Serial.println("Calibration in progress...");
+
+  MQ131.calibrate();
+
+  Serial.println("Calibration done!");
+  Serial.print("R0 = ");
+  Serial.print(MQ131.getR0());
+  Serial.println(" Ohms");
+  Serial.print("Time to heat = ");
+  Serial.print(MQ131.getTimeToRead());
+  Serial.println(" s");
+
   bool bme_status;
 
   for (int i = 0; i < ANGLE_AVGS; i++)
@@ -530,6 +552,21 @@ void setup()
 
 void loop()
 {
+  Serial.println("Sampling...");
+  MQ131.sample();
+  Serial.print("Concentration O3 : ");
+  Serial.print(MQ131.getO3(PPM));
+  Serial.println(" ppm");
+  Serial.print("Concentration O3 : ");
+  Serial.print(MQ131.getO3(PPB));
+  Serial.println(" ppb");
+  Serial.print("Concentration O3 : ");
+  Serial.print(MQ131.getO3(MG_M3));
+  Serial.println(" mg/m3");
+  Serial.print("Concentration O3 : ");
+  Serial.print(MQ131.getO3(UG_M3));
+  Serial.println(" ug/m3");
+
   if (digitalRead(BUTTON) == LOW)
   {
     ++button_ctr;
